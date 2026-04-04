73c14a4b35c5f939b2761a79508a8c97b2892f7f
crisalcaino99 <cristobal.alcaino@notus.cl>
2026-04-03 21:54:29 -0300

    todo changes

---
+++ src/services/hard_reset.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+# import shutil
+from pathlib import Path
+
+
+def delete_database(path_to_db: Path) -> bool:
+    # Include specific database checking
+    return True
+
+# Probably change to delete folder and nothing else
+def delete_downloads_folder(path_to_downloads: Path) -> bool:
+
+    return True
+
+def delete_covers_folder(path_to_covers: Path) -> bool:
+
+    return True
+
+def delete_exports_folder(path_to_exports: Path) -> bool:
+
+    return True
+
+def hard_reset(path_to_db: Path, path_to_downloads: Path,
+            path_to_covers: Path, path_to_exports: Path):
+
+    return True
+
+
+
